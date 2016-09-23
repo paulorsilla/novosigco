b01a3c61c06ebc4801b0f56b8d8dd7801928c2ba
AnDr33wS <williamgerenuttidm@gmail.com>
2016-09-23 11:34:51 -0300

    Começo de implementação de turma.

---
+++ module/Application/src/Application/Model/TurmaInstrutor.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Application\Model;
+
+use Core\Model\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Entidade TurmaInstrutor
+ *
+ * @category Application
+ * @package Model
+ *         
+ *          @ORM\Entity
+ *          @ORM\Table(name="turma_instrutor")
+ *         
+ */
+class TurmaInstrutor extends Entity {
+	
+	/**
+	 * @ORM\Column(type="integer" name="instrutor_id")
+	 */
+	protected $instrutor;
+	
+	/**
+	 * @ORM\Column(type="integer" name="turma_id")
+	 */
+	protected $turma;
+	public function getInstrutor() {
+		return $this->instrutor;
+	}
+	public function setInstrutor($instrutor) {
+		$this->instrutor = $instrutor;
+	}
+	public function getTurma() {
+		return $this->turma;
+	}
+	public function setTurma($turma) {
+		$this->turma = $turma;
+	}
+	
+}
